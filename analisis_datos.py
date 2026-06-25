27d97f00bdb1ff44f83937bb2a03d12a0fa0a604
rrivas <rrivasr12@gmail.com>
2026-06-25 01:40:20 -0400

    first commit

---
+++ analisis_datos.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.4
+#   kernelspec:
+#     display_name: Python (proyecto)
+#     language: python
+#     name: proyecto
+# ---
+
+# %%
